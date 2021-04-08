98be9553b243a143c5b7fde294ce23455f4d6100
Dexter <merino-@hotmail.es>
2021-04-08 15:52:03 -0500

    Agregar archivos de proyecto.

---
+++ SalesSystem/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Areas.Users.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        [Area("Users")] // especificamos al controlador el area en que se encuentra
+        // esto se hace cada vez que se agrega un controller
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
